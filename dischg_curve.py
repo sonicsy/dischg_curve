33c45710d63fa22d6aec38dc6ec4e11a672e2dad
sonicss <17595634@qq.com>
2025-09-29 15:58:08 +0800

    更新了亿纬314Ah电芯0.5P放电测试数据

--- dischg_curve.py
+++ dischg_curve.py
@@ -28,17 +28,43 @@ import plotly.express as px
 
 # df_merged = pd.concat([df1_resampled, df2_resampled, df3_resampled], axis=1)
 
-st.set_page_config(layout="wide")
 
 TOTAL_POINT = 1000
-soc_points = np.linspace(0, TOTAL_POINT, TOTAL_POINT + 1, dtype=int)
-soc_index = soc_points / TOTAL_POINT * 100
+st.set_page_config(layout="wide")
+warnings.filterwarnings("ignore")
 
-df_merged = pd.read_csv('resampled_data.csv')
-df_merged['SOC'] = soc_index
-df_merged.set_index('SOC', inplace=True)
+# -*- coding: utf-8 -*-
+"""
+数据加载模块
+"""
+import pandas as pd
 
 
-fig = px.line(df_merged, title='海辰280Ah放电和OCV_SOC曲线', color_discrete_sequence=['red', 'green', 'blue', 'purple'], width=1500, height=600)
-fig.update_layout(xaxis_title='SOC（%）', yaxis_title='电压(V)', xaxis_dtick=5)
-st.plotly_chart(fig, use_container_width=True)
+def load_data() -> pd.DataFrame:
+    df_merged = pd.read_csv('resampled_data.csv')
+    return df_merged
+
+def get_soc_index():
+    soc_points = np.linspace(0, TOTAL_POINT, TOTAL_POINT + 1, dtype=int)
+    soc_index = soc_points / TOTAL_POINT * 100
+    return soc_index, soc_points
+
+
+
+def show_fig(df_merged: pd.DataFrame, reversed: bool = False):
+    fig = px.line(df_merged, title='LFP储能电芯放电和OCV_SOC曲线', color_discrete_sequence=['red', 'green', 'blue', 'purple', 'orange'], width=1500, height=600)
+    fig.update_layout(xaxis_title='SOC（%）', yaxis_title='电压(V)', xaxis_dtick=5)
+    if reversed:
+        fig.update_xaxes(autorange="reversed")
+    fig.update_yaxes(range=[2.5, 3.65])
+    st.plotly_chart(fig, use_container_width=True)
+    # return fig
+
+if __name__ == "__main__":
+    soc_index, soc_points = get_soc_index()
+    df_merged = load_data()
+    # df_merged.drop(columns=["Unnamed: 0"], inplace=True)
+    df_merged['SOC'] = soc_index
+    df_merged.set_index('SOC', inplace=True)
+    df_merged = df_merged[['亿纬314Ah0.5P放电电压1', '亿纬314Ah0.5P放电电压2']]
+    show_fig(df_merged, reversed=True)
